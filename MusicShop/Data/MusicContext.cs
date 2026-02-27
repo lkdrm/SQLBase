ab87e44ac26ff1fc0378fdf87204ca15308b96e6
LkDrm <larik115@gmail.com>
2026-02-27 12:28:13 +0100

    Add base prop for handling with WPF
    Signed-off-by: LkDrm <larik115@gmail.com>

--- MusicShop/Data/MusicContext.cs
+++ MusicShop/Data/MusicContext.cs
@@ -4,7 +4,6 @@ namespace MusicShop.Data;
 
 public class MusicContext : DbContext
 {
-
     public DbSet<Song> Songs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=> optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MusicShopDb;Trusted_Connection=True;");
--- MusicShop/MainWindow.xaml.cs
+++ MusicShop/MainWindow.xaml.cs
@@ -1,24 +1,90 @@
-﻿using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicShop.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace MusicShop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += async (s, e) => await LoadSongsAsync();
+        }
+
+        private async Task LoadSongsAsync()
+        {
+            using var db = new MusicContext();
+            await db.Database.EnsureCreatedAsync();
+
+            var songs = await db.Songs.ToListAsync();
+
+            SongsGrid.ItemsSource = songs;
+        }
+
+        private async void AddSongs_Click(object sender, RoutedEventArgs e)
+        {
+            using var db = new MusicContext();
+            if (!await db.Songs.AnyAsync())
+            {
+                db.Songs.Add(new Song { Title = "Bohemian Rhapsody", Artist = "Queen", Album = "A Night at the Opera", Price = 1.99 });
+                db.Songs.Add(new Song { Title = "Shape of You", Artist = "Ed Sheeran", Album = "Divide", Price = 1.29 });
+                db.Songs.Add(new Song { Title = "Ой у лузі червона калина", Artist = "Бумбокс", Album = "Single", Price = 0.99 });
+
+                await db.SaveChangesAsync();
+
+                await LoadSongsAsync();
+                await ShowMessageAsync("Songs successfully added", "Song add");
+            }
+            else
+            {
+                await ShowMessageAsync("Is in the data base", "DataBase");
+            }
+        }
+
+        private async void BuySong_click(object sender, RoutedEventArgs e)
+        {
+            if (SongsGrid.SelectedItem is Song selectedSong)
+            {
+                await ShowMessageAsync($"U purchase: {selectedSong.Artist} - {selectedSong.Title}\n You paid: {selectedSong.Price}$", "Buy");
+            }
+            else
+            {
+                await ShowMessageAsync("Pick song from the list", "Buy");
+            }
+        }
+
+        private async void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentTheme = ApplicationThemeManager.GetAppTheme();
+            ApplicationThemeManager.Apply(currentTheme == ApplicationTheme.Dark ? ApplicationTheme.Light : ApplicationTheme.Dark);
+        }
+
+        private async void SearchBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            string searchText = SearchBox.Text.ToLower().Trim();
+
+            using var db = new MusicContext();
+            var allsongs = await db.Songs.ToListAsync();
+
+            var filtertedSongs = allsongs.Where(s => s.Title.ToLower().Contains(searchText) || s.Artist.ToLower().Contains(searchText));
+
+            SongsGrid.ItemsSource = filtertedSongs;
+        }
+
+        private static async Task ShowMessageAsync(string message, string title)
+        {
+            var box = new Wpf.Ui.Controls.MessageBox()
+            {
+                Title = title,
+                Content = message
+            };
+            await box.ShowDialogAsync();
         }
     }
 }
